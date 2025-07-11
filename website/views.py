0bcc04bf3a67bb1cbe5d394ec36167ccb061de39
Álvaro Yuri <alvaro.yuri@outlook.com>
2025-07-11 15:21:41 -0300

    últimas alterações feitas no notebook

--- website/views.py
+++ website/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "website/index.html")
+
+def sobre(request):
+    return(render(request, "website/sobre.html"))
+
+def post(request):
+    return(render(request, "website/post.html"))
+
+def contato(request):
+    return(render(request, "website/post.html"))
+
+"""""
+def index(request):
+    postagens = [
+        {
+            "id": 1,
+            "titulo": "Minha primeira postagem",
+            "subtitulo": "Nessa postagem o conteúdo é muito importante.",
+            "autor": "James Wilson",
+            "data": "27 de junho de 2025"
+        },
+        {
+            "id": 2,
+            "titulo": "Minha segunda postagem",
+            "subtitulo": "Outra postagem interessante.",
+            "autor": "Ana Souza",
+            "data": "28 de junho de 2025"
+        }
+    ]
+    return render(request, "website/index.html", {"postagens": postagens})
+
+"""""
+
